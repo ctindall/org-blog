a86eccadeaf573dd5054f3b424d282b2c6adb13a
Cameron Tindall <ctindall@gmail.com>
2018-03-14 13:44:37 -0500

    cutting release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='orgblog',
-      version='0.7.1',
+      version='0.7.2',
       description='easy web publishing with org-mode files',
       url='http://github.com/ctindall/org-blog',
       author='Cameron Tindall',
